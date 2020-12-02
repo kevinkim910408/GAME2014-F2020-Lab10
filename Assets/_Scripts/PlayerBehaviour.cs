222e950738099517fab534945895bd609a050c8d
kevinkim910408 <kevinkim0408@naver.com>
2020-12-02 16:52:07 +0900

    Initial commit

---
+++ Assets/_Scripts/PlayerBehaviour.cs
@@ -0,0 +1,308 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using Unity.Mathematics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Cinemachine;
+
+public class PlayerBehaviour : MonoBehaviour
+{
+    [Header("Controls")]
+    public Joystick joystick;
+    public float joystickHorizontalSensitivity;
+    public float joystickVerticalSensitivity;
+    public float horizontalForce;
+    public float verticalForce;
+
+    [Header("Platform Detection")]
+    public bool isGrounded;
+    public bool isJumping;
+    public bool isCrouching;
+    public Transform spawnPoint;
+    public Transform lookAheadPoint;
+    public Transform lookInFrontPoint;
+    public LayerMask collisionGroundLayer;
+    public LayerMask collisionWallLayer;
+    public RampDirection rampDirection;
+    public bool onRamp;
+    public float rampForceSensitivity;
+
+    [Header("Player Abilities")] 
+    public int health;
+    public int lives;
+    public BarController healthBar;
+    public Animator livesHUD;
+
+    [Header("Audio")]
+    public AudioSource jumpSound;
+    public AudioSource[] hitSounds;
+    public AudioSource dieSound;
+
+    [Header("Special FX")]
+    public CinemachineVirtualCamera vcam1;
+    public CinemachineBasicMultiChannelPerlin perlin;
+    public float maxShakeTime;
+    public float shakeTimer;
+    public float shakeIntensity;
+    public bool isCameraShaking = false;
+
+    private Rigidbody2D m_rigidBody2D;
+    private SpriteRenderer m_spriteRenderer;
+    private Animator m_animator;
+    private RaycastHit2D groundHit;
+    private ParticleSystem m_dustTrail;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+        lives = 3;
+        maxShakeTime = 0.3f;
+
+        shakeTimer = maxShakeTime;
+
+        m_rigidBody2D = GetComponent<Rigidbody2D>();
+        m_spriteRenderer = GetComponent<SpriteRenderer>();
+        m_animator = GetComponent<Animator>();
+        m_dustTrail = GetComponentInChildren<ParticleSystem>();
+
+        //for screen shake
+        vcam1 = FindObjectOfType<CinemachineVirtualCamera>();
+        perlin = vcam1.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        _LookInFront();
+        _LookAhead();
+        _Move();
+
+        if(isCameraShaking == true)
+        {
+            shakeTimer -= Time.deltaTime;
+
+            if(shakeTimer <= 0.0f)
+            {
+                perlin.m_AmplitudeGain = 0.0f;
+                isCameraShaking = false;
+                shakeTimer = maxShakeTime;
+            }
+        }
+    }
+
+    private void _LookInFront()
+    {
+        if (!isGrounded)
+        {
+            rampDirection = RampDirection.NONE;
+            return;
+        }
+
+        var wallHit = Physics2D.Linecast(transform.position, lookInFrontPoint.position, collisionWallLayer);
+        if (wallHit && isOnSlope())
+        {
+            rampDirection = RampDirection.UP;
+        }
+        else if (!wallHit && isOnSlope())
+        {
+            rampDirection = RampDirection.DOWN;
+        }
+
+        Debug.DrawLine(transform.position, lookInFrontPoint.position, Color.red);
+    }
+
+    private void _LookAhead()
+    {
+        groundHit = Physics2D.Linecast(transform.position, lookAheadPoint.position, collisionGroundLayer);
+
+        isGrounded = (groundHit) ? true : false;
+
+        Debug.DrawLine(transform.position, lookAheadPoint.position, Color.green);
+    }
+
+    private bool isOnSlope()
+    {
+        if (!isGrounded)
+        {
+            onRamp = false;
+            return false;
+        }
+
+        if (groundHit.normal != Vector2.up)
+        {
+            onRamp = true;
+            return true;
+        }
+
+        onRamp = false;
+        return false;
+    }
+
+    void _Move()
+    {
+        if (isGrounded)
+        {
+            if (!isJumping && !isCrouching)
+            {
+                if (joystick.Horizontal > joystickHorizontalSensitivity)
+                {
+                    // move right
+                    m_rigidBody2D.AddForce(Vector2.right * horizontalForce * Time.deltaTime);
+                    transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+                    if (onRamp && rampDirection == RampDirection.UP)
+                    {
+                        m_rigidBody2D.AddForce(Vector2.up * horizontalForce * rampForceSensitivity * Time.deltaTime);
+                    }
+                    else if (onRamp && rampDirection == RampDirection.DOWN)
+                    {
+                        m_rigidBody2D.AddForce(Vector2.down * horizontalForce * rampForceSensitivity * Time.deltaTime);
+                    }
+
+
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
+
+                    CreateDustTrail();
+                }
+                else if (joystick.Horizontal < -joystickHorizontalSensitivity)
+                {
+                    // move left
+                    m_rigidBody2D.AddForce(Vector2.left * horizontalForce * Time.deltaTime);
+                    transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+                    if (onRamp && rampDirection == RampDirection.UP)
+                    {
+                        m_rigidBody2D.AddForce(Vector2.up * horizontalForce * 0.5f * Time.deltaTime);
+                    }
+                    else if (onRamp && rampDirection == RampDirection.DOWN)
+                    {
+                        m_rigidBody2D.AddForce(Vector2.down * horizontalForce * 0.5f * Time.deltaTime);
+                    }
+
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.RUN);
+
+                    CreateDustTrail();
+                }
+                else
+                {
+                    m_animator.SetInteger("AnimState", (int)PlayerAnimationType.IDLE);
+                }
+            }
+
+            if ((joystick.Vertical > joystickVerticalSensitivity) && (!isJumping))
+            {
+                // jump
+                m_rigidBody2D.AddForce(Vector2.up * verticalForce);
+                m_animator.SetInteger("AnimState", (int) PlayerAnimationType.JUMP);
+                isJumping = true;
+
+                jumpSound.Play(); //impulse sound
+                CreateDustTrail();
+            }
+            else
+            {
+                isJumping = false;
+            }
+
+            if ((joystick.Vertical < -joystickVerticalSensitivity) && (!isCrouching))
+            {
+                m_animator.SetInteger("AnimState", (int)PlayerAnimationType.CROUCH);
+                isCrouching = true;
+            }
+            else
+            {
+                isCrouching = false;
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        // respawn
+        if (other.gameObject.CompareTag("DeathPlane"))
+        {
+            LoseLife();
+        }
+
+        if (other.gameObject.CompareTag("Bullet"))
+        {
+            TakeDamage(10);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Enemy"))
+        {
+            TakeDamage(15);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            // delay bullet firing 
+            if (Time.frameCount % 20 == 0)
+            {
+                TakeDamage(5);
+            }
+        }
+    }
+
+    public void LoseLife()
+    {
+        lives -= 1;
+
+        livesHUD.SetInteger("LivesState", lives);
+
+        dieSound.Play();
+
+        if (lives > 0)
+        {
+            health = 100;
+            healthBar.SetValue(health);
+            transform.position = spawnPoint.position;
+        }
+        else
+        {
+            SceneManager.LoadScene("End");
+        }
+        
+    }
+
+    public void TakeDamage(int damage)
+    {
+        health -= damage;
+        healthBar.SetValue(health);
+
+        //play hit sound here
+        PlayHitSound();
+        ShakeCamera();
+
+        if (health <= 0)
+        {
+            LoseLife();
+        }
+    }
+
+    private void CreateDustTrail()
+    {
+        m_dustTrail.GetComponent<Renderer>().material.SetColor("_Color", Color.red);
+
+        m_dustTrail.Play();
+    }
+
+    private void PlayHitSound()
+    {
+        var randomHitSound = hitSounds[Random.Range(0, 3)];
+        randomHitSound.Play();
+    }
+
+    private void ShakeCamera()
+    {
+        perlin.m_AmplitudeGain = shakeIntensity;
+
+        isCameraShaking = true;
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# GAME2014MobileGameDevelopment1_Lab10-main
+ GAME2014MobileGameDevelopment1_Lab10-main
